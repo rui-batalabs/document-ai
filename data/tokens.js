2065c2cf0253e429a8fc6e23da0b4ec68707e544
rui-batalabs <rui@batalabs.com>
2024-12-10 18:38:19 -0500

    fix: Resolved syntax and logic issues in tokens module
    - Corrected typo in `async` declaration for `getAllTokens` function.

--- data/tokens.js
+++ data/tokens.js
@@ -1,66 +1,68 @@
-import { ObjectId, ReturnDocument } from 'mongodb';
-import { passwordTokens } from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import {passwordTokens} from '../config/mongoCollections.js';
 import helper from '../serverSideHelpers.js';
-import e from 'express';
 
 const exportedMethods = {
 
-    asnyc getAllTokens(){
+    async getAllTokens() {
         const tokenCollection = await passwordTokens();
-        const tokens = tokenCollection.find({}).toArray();
-        if(!tokens) throw 'There are no valid password reset tokens';
-        tokens = tokens.map((element) => {  
-            element._id = element._id.toString();  
-            return element; 
-          });
+        let tokens = await tokenCollection.find({}).toArray();
+        if (!tokens) throw new Error('There are no valid password reset tokens');
+
+        tokens = tokens.map((element) => {
+            element._id = element._id.toString();
+            return element;
+        });
 
         return tokens;
     },
 
-    async updateTokenExpiration(token){
+    async updateTokenExpiration(token) {
         const tokenCollection = await passwordTokens();
         const userToken = await tokenCollection.findOneAndUpdate(
-            {token: token, accessed:false},
-            {$set: {time_created: new Date(), time_expired: new Date(Date.now() +15*60000)}},
-            {returnDocument:'after'}
+            {token: token, accessed: false},
+            {$set: {time_created: new Date(), time_expired: new Date(Date.now() + 15 * 60000)}},
+            {returnDocument: 'after'}
         );
 
-        if(!userToken) throw 'No token found';
-
+        if (!userToken.value) throw new Error('No token found');
+        return userToken.value;
     },
 
-
-    async createToken(email){
-        //generate token, accessed = false, id, timecreated + timeexpired
+    async createToken(email) {
+        // Generate token, accessed = false, id, time created + time expired
         const token = helper.tokenGenerator();
         email = helper.emailCheck(email);
 
         const addToken = {
-            email:email,
-            token:token,
-            accessed:false,
-            time_created:new Date(Date.now()),
-            time_expired: new Date(Date.now() + 1000*60)
-        }
-        const tokenCollection = await tokens();
-        const ifToken = await tokenCollection.find({email:email});
-        if(ifToken) {
-             const newToken = await tokenCollection.findOneAndUpdate({email:email},
+            email: email,
+            token: token,
+            accessed: false,
+            time_created: new Date(),
+            time_expired: new Date(Date.now() + 1000 * 60),
+        };
+
+        const tokenCollection = await passwordTokens();
+        const existingToken = await tokenCollection.findOne({email: email});
+
+        let newToken;
+        if (existingToken) {
+            newToken = await tokenCollection.findOneAndUpdate(
+                {email: email},
                 {$set: addToken},
-                {returnDocument:'after'}
-             );
-    }
-        else{
-            const newToken = await tokenCollection.insertOne(addToken);
+                {returnDocument: 'after'}
+            );
+        } else {
+            const insertResult = await tokenCollection.insertOne(addToken);
+            if (insertResult.acknowledged) {
+                newToken = await tokenCollection.findOne({_id: insertResult.insertedId});
+            }
         }
-        if(!newToken) throw 'Error adding token';
+
+        if (!newToken) throw new Error('Error adding token');
         return newToken;
-}
+    },
 
+};
 
-
-
-
-
-}
 export default exportedMethods;
--- data/users.js
+++ data/users.js
@@ -1,128 +1,129 @@
-import { ObjectId } from 'mongodb';
-import { users } from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import {users} from '../config/mongoCollections.js';
 import helper from '../serverSideHelpers.js';
 import e from 'express';
 import bcrypt from 'bcryptjs';
 
 
 const exportedMethods = {
-  async addUser(username, email, password, hashed_password){
+    async addUser(username, email, password, hashed_password) {
 
-  username = helper.usernameCheck(username);
-  email = helper.emailCheck(email);
-  password = helper.passwordCheck(password);
-  hashed_password = await helper.hashedPasswordCheck(password, hashed_password)
-
-  
-
-  const usersCollection = await users();
-
-  // Ensure username and email are unique
-  const existingUser = await usersCollection.findOne({
-    $or: [{ username: username.toLowerCase() }, { email: email.toLowerCase() }],
-  });
-
-  if (existingUser) {
-    throw new Error('Username or email already exists.');
-  }
-
-  const newUser = {
-    username: username.toLowerCase(),
-    email: email.toLowerCase(),
-    hashed_password: hashed_password,
-    profile_picture: '',
-    uploaded_docs: [],
-    queries: [],
-  };
-
-  const insertResult = await usersCollection.insertOne(newUser);
-  if (!insertResult.acknowledged || !insertResult.insertedId) {
-    throw new Error('Could not add user to the database.');
-  }
-
-  return {
-    insertedId: insertResult.insertedId.toString(),
-    ...newUser,
-  };
-},
+        username = helper.usernameCheck(username);
+        email = helper.emailCheck(email);
+        password = helper.passwordCheck(password);
+        hashed_password = await helper.hashedPasswordCheck(password, hashed_password)
 
 
+        const usersCollection = await users();
 
-async getAllUsers(){
-  const usersCollection = await users();
-  let usersList = await usersCollection.find({}).toArray();
-  if(!usersList) throw 'Could not get all users';
-  usersList = usersList.map((element) => {  
-      element._id = element._id.toString();  
-      return element; 
-    });
-    return usersList; 
-},
+        // Ensure username and email are unique
+        const existingUser = await usersCollection.findOne({
+            $or: [{username: username.toLowerCase()}, {email: email.toLowerCase()}],
+        });
 
-async addUserDocument(email, document_id){
-    email = helper.emailCheck(email);
-    if(!document_id) throw 'no document id';
-    if (!ObjectId.isValid(document_id)) {
-      throw new Error('invalid object ID');
-    };
-    const usersCollection = await users();
-    const updatedUser = await usersCollection.findOneAndUpdate(
+        if (existingUser) {
+            throw new Error('Username or email already exists.');
+        }
+
+        const newUser = {
+            username: username.toLowerCase(),
+            email: email.toLowerCase(),
+            hashed_password: hashed_password,
+            profile_picture: '',
+            uploaded_docs: [],
+            queries: [],
+        };
+
+        const insertResult = await usersCollection.insertOne(newUser);
+        if (!insertResult.acknowledged || !insertResult.insertedId) {
+            throw new Error('Could not add user to the database.');
+        }
+
+        return {
+            insertedId: insertResult.insertedId.toString(),
+            ...newUser,
+        };
+    },
+
+
+    async getAllUsers() {
+        const usersCollection = await users();
+        let usersList = await usersCollection.find({}).toArray();
+        if (!usersList) throw 'Could not get all users';
+        usersList = usersList.map((element) => {
+            element._id = element._id.toString();
+            return element;
+        });
+        return usersList;
+    },
+
+    async addUserDocument(email, document_id) {
+        email = helper.emailCheck(email);
+        if (!document_id) throw 'no document id';
+        if (!ObjectId.isValid(document_id)) {
+            throw new Error('invalid object ID');
+        }
+        ;
+        const usersCollection = await users();
+        const updatedUser = await usersCollection.findOneAndUpdate(
             {email: email},
             {$push: {uploaded_docs: document_id}},
             {returnDocument: 'after'}
-    )
-    if(!updatedUser) throw 'error adding document id to user documents';
-    return updatedUser;
-},
+        )
+        if (!updatedUser) throw 'error adding document id to user documents';
+        return updatedUser;
+    },
 
-async addUserQuery(email, query_id){
-  email = helper.emailCheck(email);
-  if(!query_id) throw 'no document id';
-  if (!ObjectId.isValid(query_id)) {
-    throw new Error('invalid object ID');
-  };
-  const usersCollection = await users();
-  const updatedUser = await usersCollection.findOneAndUpdate(
-          {email: email},
-          {$push: {queries: query_id}},
-          {returnDocument: 'after'}
-  )
-  if(!updatedUser) throw 'error adding document id to user documents';
-  return updatedUser;
-},
+    async addUserQuery(email, query_id) {
+        email = helper.emailCheck(email);
+        if (!query_id) throw 'no document id';
+        if (!ObjectId.isValid(query_id)) {
+            throw new Error('invalid object ID');
+        }
+        ;
+        const usersCollection = await users();
+        const updatedUser = await usersCollection.findOneAndUpdate(
+            {email: email},
+            {$push: {queries: query_id}},
+            {returnDocument: 'after'}
+        )
+        if (!updatedUser) throw 'error adding document id to user documents';
+        return updatedUser;
+    },
 
-async deleteUserFile(email, fileId){
-  email = helper.emailCheck(email);
-  if(!fileId) throw 'no document id';
-  if (!ObjectId.isValid(fileId)) {
-    throw new Error('invalid object ID');
-  };
-  const usersCollection = await users();
-  const updatedUser = await usersCollection.findOneAndUpdate(
-          {email: email},
-          {$pull: {uploaded_docs: new ObjectId(fileId)}},
-          {returnDocument: 'after'}
-  )
-  if(!updatedUser) throw 'error adding document id to user documents';
-  return updatedUser;
-}
+    async deleteUserFile(email, fileId) {
+        email = helper.emailCheck(email);
+        if (!fileId) throw 'no document id';
+        if (!ObjectId.isValid(fileId)) {
+            throw new Error('invalid object ID');
+        }
+        ;
+        const usersCollection = await users();
+        const updatedUser = await usersCollection.findOneAndUpdate(
+            {email: email},
+            {$pull: {uploaded_docs: new ObjectId(fileId)}},
+            {returnDocument: 'after'}
+        )
+        if (!updatedUser) throw 'error adding document id to user documents';
+        return updatedUser;
+    },
 
-async changeUserPassword(email, password, confirmPassword){
-  email = helper.emailCheck(email);
-  password = helper.passwordCheck(password);
-  confirmPassword = helper.passwordCheck(confirmPassword);
-  if(password!==confirmPassword)throw 'These passwords do not match'
-  const hashed_password = await bcrypt.hash(password,10)
-  const userCollection = await users();
-  const updatedPassword = await userCollection.findOneAndUpdate(
-    {email:email},
-    {$set: {password:hashed_password}},
-    {returnDocument:'after'}
-  )
-  if(!updatedPassword) throw 'Error updating password';
-  return updatedPassword;
+    async changeUserPassword(email, password, confirmPassword) {
+        email = helper.emailCheck(email);
+        password = helper.passwordCheck(password);
+        confirmPassword = helper.passwordCheck(confirmPassword);
+        if (password !== confirmPassword) throw 'These passwords do not match'
+        const hashed_password = await bcrypt.hash(password, 10)
+        const userCollection = await users();
+        const updatedPassword = await userCollection.findOneAndUpdate(
+            {email: email},
+            {$set: {password: hashed_password}},
+            {returnDocument: 'after'}
+        )
+        if (!updatedPassword) throw 'Error updating password';
+        return updatedPassword;
 
-}
+    }
 
 };
 
